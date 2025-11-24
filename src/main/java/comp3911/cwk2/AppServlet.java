2a349f7142922f4f5327c75b793a08396d24befc
Andrew Auld <sc23aam@leeds.ac.uk>
2025-11-24 14:33:47 +0000

    andrew completed fix2

--- src/main/java/comp3911/cwk2/AppServlet.java
+++ src/main/java/comp3911/cwk2/AppServlet.java
@@ -30,7 +30,7 @@ public class AppServlet extends HttpServlet {
 
   private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
   private static final String AUTH_QUERY = "select * from user where username=? and password=?"; // fixes for 1.1
-  private static final String SEARCH_QUERY = "select * from patient where surname='%s' collate nocase";
+  private static final String SEARCH_QUERY = "select * from patient where surname=? collate nocase"; // fixes for 1.2
 
   private final Configuration fm = new Configuration(Configuration.VERSION_2_3_28);
   private Connection database;
@@ -116,11 +116,12 @@ public class AppServlet extends HttpServlet {
     }
   }
 
+  // fixes for 1.2 (using preparedStatement)
   private List<Record> searchResults(String surname) throws SQLException {
     List<Record> records = new ArrayList<>();
-    String query = String.format(SEARCH_QUERY, surname);
-    try (Statement stmt = database.createStatement()) {
-      ResultSet results = stmt.executeQuery(query);
+    try (PreparedStatement stmt = database.prepareStatement(SEARCH_QUERY)) {
+      stmt.setString(1, surname);
+      ResultSet results = stmt.executeQuery();
       while (results.next()) {
         Record rec = new Record();
         rec.setSurname(results.getString(2));
